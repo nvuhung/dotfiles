ab293e733aa64d03bf15b9e5bcb6ff10bca3c57b
nvuhung <nvuhung@outlook.com>
2021-05-08 18:43:27 +0700

    Use prettierd to speedup format

--- .config/nvim/lua/efm/eslint.lua
+++ .config/nvim/lua/efm/eslint.lua
@@ -1,9 +1,17 @@
 return {
-    lintCommand = "eslint_d -f unix --stdin",
-    lintFormats = {"%f:%l:%c: %m"},
-    formatCommand = "eslint_d --fix-to-stdout --stdin",
-    formatStdin = true,
-    lintIgnoreExitCode = true,
+    lintCommand = 'eslint_d -f visualstudio --stdin --stdin-filename ${INPUT}',
+    lintSource = 'eslint_d',
     lintStdin = true,
-    codeAction = true
+    lintFormats = {'%f(%l,%c): %tarning %m', '%f(%l,%c): %rror %m'},
+    lintIgnoreExitCode = true,
+    formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename ${INPUT}',
+    formatStdin = true
+
+    -- lintCommand = "eslint_d -f unix --stdin",
+    -- lintFormats = {"%f:%l:%c: %m"},
+    -- formatCommand = "eslint_d --fix-to-stdout --stdin",
+    -- formatStdin = true,
+    -- lintIgnoreExitCode = true,
+    -- lintStdin = true,
+    -- codeAction = true
 }
--- .config/nvim/lua/efm/prettier.lua
+++ .config/nvim/lua/efm/prettier.lua
@@ -1,12 +1,15 @@
 return {
-    formatCommand = ([[
-        ./node_modules/.bin/prettier
-        ${--config-precedence:configPrecedence}
-        ${--tab-width:tabWidth}
-        ${--single-quote:singleQuote}
-        ${--trailing-comma:trailingComma}
-    ]]):gsub(
-        "\n",
-        ""
-    )
+    -- npm install -g @fsouza/prettierd
+    formatCommand = 'prettierd ${INPUT}', formatStdin = true
+
+    -- formatCommand = ([[
+    --     ./node_modules/.bin/prettier
+    --     ${--config-precedence:configPrecedence}
+    --     ${--tab-width:tabWidth}
+    --     ${--single-quote:singleQuote}
+    --     ${--trailing-comma:trailingComma}
+    -- ]]):gsub(
+    --     "\n",
+    --     ""
+    -- )
 }
