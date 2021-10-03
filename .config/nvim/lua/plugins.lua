4a5f9997634c8b6e64d43011461904a1e0b7f915
nvuhung <nvuhung@outlook.com>
2021-10-03 17:11:17 +0700

    Update plugins nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,17 +1,5 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
-end
-
 -- Only required if you have packer in your `opt` pack
 vim.cmd('packadd packer.nvim')
-
--- vim.cmd [[ autocmd BufWritePost packed.lua PackerCompile ]]
 
 return require('packer').startup(
   function()
@@ -91,7 +79,6 @@ return require('packer').startup(
 
     -- File explorer
     use {'kyazdani42/nvim-tree.lua', config = require'plugins.nvim-tree'}
-
     -- Smooth scroll
     use {'psliwka/vim-smoothie'}
 
