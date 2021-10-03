5ba93c057382f626e7bed88fbcd2caa53966398f
nvuhung <nvuhung@outlook.com>
2021-10-03 17:11:56 +0700

    Upgrade nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -9,15 +9,15 @@ return function()
   vim.g.nvim_tree_side = 'left'
   vim.g.nvim_tree_width = 30
   vim.g.nvim_tree_ignore = {".git", ".DS_Store", ".cache", "node_modules"}
-  vim.g.nvim_tree_auto_open = 1
-  vim.g.nvim_tree_auto_close = 0
+  -- vim.g.nvim_tree_auto_open = 1
+  -- vim.g.nvim_tree_auto_close = 0
   vim.g.nvim_tree_quit_on_open = 0
-  vim.g.nvim_tree_follow = 1
+  -- vim.g.nvim_tree_follow = 1
   vim.g.nvim_tree_indent_markers = 0
   vim.g.nvim_tree_hide_dotfiles = 0
   vim.g.nvim_tree_git_hl = 1
   vim.g.nvim_tree_root_folder_modifier = ':~'
-  vim.g.nvim_tree_tab_open = 1
+  -- vim.g.nvim_tree_tab_open = 1
   vim.g.nvim_tree_width_allow_resize  = 0
   vim.g.nvim_tree_show_icons = {
     git = 1,
@@ -46,9 +46,8 @@ return function()
   
   local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-  vim.g.nvim_tree_bindings = {
-    -- { key = "|",                          cb = tree_cb("vsplit") },
-    -- { key = "_",                          cb = tree_cb("split") },
+  local list = {
+    -- { key = {"<CR>", "o" }, cb = ":lua some_func()<cr>", mode = "n"}
     { key = "h",                          cb = tree_cb("close_node") },
     { key = "<s-cr>",                     cb = tree_cb("close_node") },
     { key = "<BS>",                       cb = tree_cb("close_node") },
@@ -57,4 +56,65 @@ return function()
     { key = "o",                          cb = tree_cb("edit") },
   }
   
+  require('nvim-tree').setup({
+    -- disables netrw completely
+    disable_netrw       = true,
+    -- hijack netrw window on startup
+    hijack_netrw        = true,
+    -- open the tree when running this setup function
+    open_on_setup       = true,
+    -- will not open on setup if the filetype is in this list
+    ignore_ft_on_setup  = {},
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close          = false,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab         = false,
+    -- hijacks new directory buffers when they are opened.
+    update_to_buf_dir   = {
+      -- enable the feature
+      enable = true,
+      -- allow to open the tree if it was previously closed
+      auto_open = true,
+    },
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor       = false,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+    update_cwd          = false,
+    -- show lsp diagnostics in the signcolumn
+    lsp_diagnostics     = false,
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+      -- enables the feature
+      enable      = true,
+      -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+      -- only relevant when `update_focused_file.enable` is true
+      update_cwd  = false,
+      -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+      -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+      ignore_list = {}
+    },
+    -- configuration options for the system open command (`s` in the tree by default)
+    system_open = {
+      -- the command to run this, leaving nil should work in most cases
+      cmd  = nil,
+      -- the command arguments as a list
+      args = {}
+    },
+
+    view = {
+      -- width of the window, can be either a number (columns) or a string in `%`
+      width = 30,
+      -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+      side = 'left',
+      -- if true the tree will resize itself after opening a file
+      auto_resize = false,
+      mappings = {
+        -- custom only false will merge the list with the default mappings
+        -- if true, it will only use your list to set the mappings
+        custom_only = false,
+        -- list of mappings to set on the tree manually
+        list = list
+      }
+    }
+  })
 end
