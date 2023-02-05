89f680347aab63b905222914864e03bbf338722c
nvuhung <nvuhung@outlook.com>
2023-02-05 12:29:43 +0700

    Update comment hook

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -19,7 +19,9 @@ return {
       'JoosepAlviste/nvim-ts-context-commentstring'
     },
     config = function()
-      require('Comment').setup()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
     end
   },
 
