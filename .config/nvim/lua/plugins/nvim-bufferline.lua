880eea35d4431a6215ef4289b71e9cc2d0ca6365
nvuhung <nvuhung@outlook.com>
2023-04-03 06:00:52 +0700

    Update icons

--- .config/nvim/lua/plugins/nvim-bufferline.lua
+++ .config/nvim/lua/plugins/nvim-bufferline.lua
@@ -3,5 +3,10 @@ vim.api.nvim_set_keymap("n", "<TAB>", ":BufferLineCycleNext<CR>", { noremap = tr
 vim.api.nvim_set_keymap("n", "<S-TAB>", ":BufferLineCyclePrev<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "gb", ":BufferLinePick<CR>", { noremap = true })
 
+vim.opt.termguicolors = true
 require "bufferline".setup {
+  options = {
+    buffer_close_icon = '',
+    close_icon = '',
+  }
 }
